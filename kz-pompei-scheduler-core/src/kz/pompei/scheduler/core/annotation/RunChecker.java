014201719d5d219522794105ecf4fdd5f514a61d
Евгений Колпаков <pompei@mail.ru>
2026-05-21 19:11:15 +0500

    Added java-docs RunChecker_HMS for AI to do

--- kz-pompei-scheduler-core/src/kz/pompei/scheduler/core/annotation/RunChecker.java
+++ kz-pompei-scheduler-core/src/kz/pompei/scheduler/core/annotation/RunChecker.java
@@ -1,5 +1,17 @@
 package kz.pompei.scheduler.core.annotation;
 
+/**
+ * Interface for checking if a task should be run based on time range.
+ */
 public interface RunChecker {
-  boolean needRun(long timestampStartedAt, long timestampFrom, long timestampTo);
+
+  /**
+   * Returns true if the task should be run based on the given time range.
+   *
+   * @param timestampStartedAt milliseconds from System.currentTimeMillis() - time of start scheduler
+   * @param timestampMsFrom    milliseconds from System.currentTimeMillis() - time from
+   * @param timestampMsTo      milliseconds from System.currentTimeMillis() - time to
+   * @return true if the task should be run, false otherwise
+   */
+  boolean needRun(long timestampStartedAt, long timestampMsFrom, long timestampMsTo);
 }
--- kz-pompei-scheduler-core/src/kz/pompei/scheduler/core/annotation/RunChecker_HMS.java
+++ kz-pompei-scheduler-core/src/kz/pompei/scheduler/core/annotation/RunChecker_HMS.java
@@ -4,13 +4,30 @@ import java.util.TimeZone;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Implementation of RunChecker that checks if a task should be run based on the hour, minute, and second of any day.
+ */
 @RequiredArgsConstructor
 public class RunChecker_HMS implements RunChecker {
 
-  private final int hour, minus, second;
+  private final int hour, minute, second;
   private final @NonNull TimeZone timeZone;
 
-  @Override public boolean needRun(long timestampStartedAt, long timestampFrom, long timestampTo) {
+  /**
+   * First, the method gets (year,month,day) from `timestampStartedAt` and `timeZone` (using GregorianCalendar).
+   * <p>
+   * Then (year, month, day), `hour`, `minute`, `second` converts to milliseconds (using GregorianCalendar) to variable `timestampMs`.
+   * <p>
+   * And then returns true if `timestampMsFrom` <= `timestampMs` < `timestampMsTo`.
+   * <p>
+   * Note, the first sign contains equals, the second sign does not contain equals.
+   *
+   * @param timestampStartedAt milliseconds from System.currentTimeMillis() - time of start scheduler
+   * @param timestampMsFrom    milliseconds from System.currentTimeMillis() - time from
+   * @param timestampMsTo      milliseconds from System.currentTimeMillis() - time to
+   * @return result of check
+   */
+  @Override public boolean needRun(long timestampStartedAt, long timestampMsFrom, long timestampMsTo) {
     throw new RuntimeException("dOK428XrLM :: Not impl yet RunChecker_HMS.needRun()");
   }
 }
