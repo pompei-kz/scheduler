34a5dae361756600c26d42b228005ee36c5ad368
Евгений Колпаков <pompei@mail.ru>
2026-05-22 07:32:04 +0500

    Written empty class RunChecker_PERIODIC for AI implementation

---
+++ kz-pompei-scheduler-core/src/kz/pompei/scheduler/core/annotation/RunChecker_PERIODIC.java
@@ -0,0 +1,41 @@
+package kz.pompei.scheduler.core.annotation;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class RunChecker_PERIODIC implements RunChecker {
+
+  /**
+   * Offset in milliseconds from `timestampStartedAt` to start run. The first run should be at `timestampStartedAt + offsetMs`.
+   * <p>
+   * The next runs should be at `timestampStartedAt + offsetMs + periodMs * n`, where `n` is a non-negative integer.
+   */
+  private final long offsetMs;
+
+
+  /**
+   * Period in milliseconds between runs.
+   * The next run should be at `timestampStartedAt + offsetMs + periodMs * n`, where `n` is a non-negative integer.
+   */
+  private final long periodMs;
+
+  /**
+   *
+   *
+   * Let ts = timestampStartedAt + offsetMs + periodMs * n
+   * <p>
+   * Return true if timestampStartedAt + timestampMsFrom <= ts < timestampStartedAt + timestampMsTo
+   * <p>
+   * Otherwise false.
+   * <p>
+   * where `n` is a non-negative integer.
+   *
+   * @param timestampStartedAt milliseconds from System.currentTimeMillis() - time of start scheduler
+   * @param timestampMsFrom    milliseconds from System.currentTimeMillis() - time from
+   * @param timestampMsTo      milliseconds from System.currentTimeMillis() - time to
+   * @return true if you need to run, false otherwise
+   */
+  @Override public boolean needRun(long timestampStartedAt, long timestampMsFrom, long timestampMsTo) {
+    throw new RuntimeException("2026-05-22 07:26 Not impl yet RunChecker_PERIODIC.needRun()");
+  }
+}
